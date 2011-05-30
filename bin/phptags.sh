3ad4ef4ddf38f37625aa63d267d7cd127a6520ba
Kevin Le Brun <lebrun.k@gmail.com>
2011-05-31 01:18:50 +0200

    Generate Zend Framework ctags and some aliases

---
+++ bin/phptags.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd /usr/share/php5/ZendFramework-1.11.6/library/
+
+ctags-exuberant -f ~/.vimtags -h ".php" -R \
+--exclude="\.svn" \
+--exclude="\.git" \
+--exclude="\.hg" \
+--exclude="\.js" \
+--totals=yes \
+--tag-relative=yes \
+--PHP-kinds=+cfi \
+--regex-PHP='/abstract\s+class\s+([^ ]+)/\1/c/' \
+--regex-PHP='/interface\s+([^ ]+)/\1/i/' \
+--regex-PHP='/(public\s+|static\s+|abstract\s+|protected\s+|private\s+)+function\s+([^
+(]+)/\2/f/' \
