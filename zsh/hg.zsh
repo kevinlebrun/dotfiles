3fb84b3276cd84b752300c55b2896924a7d666c0
Kevin Le Brun <lebrun.k@gmail.com>
2012-08-08 22:17:07 +0200

    Switch to ZSH
    A fresh new zsh config
    New git and mercurial prompt
    Copy bash aliases and functions
    An amazing new prompt

---
+++ zsh/hg.zsh
@@ -0,0 +1,8 @@
+function hg_prompt_info {
+    hg prompt --angle-brackets "\
+< on %{$fg[magenta]%}<branch>%{$reset_color%}>\
+< at %{$fg[blue]%}<bookmark>%{$reset_color%}>\
+< at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
+%{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
+patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg[black]%})|post_unapplied(%{$reset_color%})>>" 2> /dev/null
+}
