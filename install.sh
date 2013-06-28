b797eb1c4ac0a750c253c0269dd21fcc946c7fe6
Kevin Le Brun <lebrun.k@gmail.com>
2013-06-28 20:15:23 +0200

    Better bin management

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@
 SMILE_PATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function install_dotfiles {
-    ln -sf ${SMILE_PATH}/vim ~/.vim
+    if [ ! -d ~/.vim ]; then
+        ln -s ${SMILE_PATH}/vim ~/.vim
+    fi
 
     for dotfile in ${SMILE_PATH}/.*
     do
@@ -13,18 +15,38 @@ function install_dotfiles {
             ln -sf $dotfile ~/$(basename $dotfile)
         fi
     done
-
-    mkdir -p ~/.ssh
-    ln -sf ${SMILE_PATH}/ssh/config ~/.ssh/config
 }
 
-function install_hg_prompt {
-    if [ ! -d ~/.hg_prompt ]; then
-        hg clone http://bitbucket.org/sjl/hg-prompt/ ~/.hg_prompt
+function hg_clone {
+    if [ ! -d "$1" ]; then
+        hg clone "$2" "$1"
     fi
 }
 
+function get_bin {
+    local binfile="$SMILE_PATH/bin/$1"
+    curl $2 -sLo "$binfile" && chmod +x "$binfile"
+}
+
 install_dotfiles
-install_hg_prompt
+
+hg_clone  ~/.hg_prompt           http://bitbucket.org/sjl/hg-prompt/
+
+get_bin   hub                    http://defunkt.io/hub/standalone # http://defunkt.io/hub/
+get_bin   ack                    http://beyondgrep.com/ack-2.04-single-file
+get_bin   cloc                   http://kent.dl.sourceforge.net/project/cloc/cloc/v1.58/cloc-1.58.pl
+get_bin   git-publish-branch     http://git-wt-commit.rubyforge.org/git-publish-branch
+get_bin   gbrt                   https://raw.github.com/benhoskings/dot-files/master/files/bin/gbrt
+get_bin   git-cwd-info           https://raw.github.com/topfunky/zsh-simple/master/bin/git-cwd-info
+get_bin   git-current-branch     https://raw.github.com/topfunky/zsh-simple/master/bin/git-current-branch
+get_bin   git-churn              https://raw.github.com/garybernhardt/dotfiles/master/bin/git-churn
+get_bin   git-divergence         https://raw.github.com/garybernhardt/dotfiles/master/bin/git-divergence
+get_bin   git-rank-contributors  http://git-wt-commit.rubyforge.org/git-rank-contributors
+get_bin   git-wtf                http://git-wt-commit.rubyforge.org/git-wtf
+get_bin   git-thanks             https://gist.github.com/rkh/74335/raw/47397a6c7fc7d6bc10eef8d619a5e752e829e5c9/git-thanks
+get_bin   git-trail              http://chneukirchen.org/dotfiles/bin/git-trail
+
+# TODO How to handle gem / brew / etc. deps ?
+# https://github.com/aanand/git-up
 
 echo "export SMILE=$SMILE_PATH" > ~/.smile.conf
