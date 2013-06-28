62bc848199b85b705882f67eca3a2b720d3ab466
Kevin Le Brun <lebrun.k@gmail.com>
2013-06-28 20:18:17 +0200

    Fix Zsh history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -3,13 +3,14 @@ HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt hist_ignore_dups # ignore duplication command history list
-setopt share_history # share command history data
+# I want to see dups to make stats
+# setopt hist_ignore_dups
+setopt share_history
 
 setopt hist_verify
 setopt inc_append_history
 setopt extended_history
-setopt hist_expire_dups_first
+# setopt hist_expire_dups_first
 setopt hist_ignore_space
 
 setopt SHARE_HISTORY
