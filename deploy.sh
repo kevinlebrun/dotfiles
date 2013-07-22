9a52e6ff213899561fea3ab96e2275af20d7bdb8
Kevin Le Brun <lebrun.k@gmail.com>
2013-07-22 19:02:04 +0200

    Final deploy script
    Update .box-name with SSH host name.
    
    Update .smile on remote servers.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,24 @@ hosts=$(cat ~/.ssh/config | sed -n '/Host\s[^*]/ p' | cut -d ' ' -f 2-)
 
 update_dotfiles() {
     local host=$1
+    local smilepath=$2
+    local destpath=$3
+
+    rsync -az --delete "$smilepath" "$host:${destpath%/*}"
+    if [ $? -eq 0 ]; then
+        echo "updated"
+        # TODO launch install without download for private servers
+        ssh -o ConnectTimeout=1 $host "$destpath/install.sh > /dev/null 2>&1" > /dev/null 2>&1
+        ssh -o ConnectTimeout=1 $host "echo $host > ~/.box-name" > /dev/null 2>&1
+        return 0
+    fi
+
+    echo "not updated, error during rsync"
+    return 1
+}
+
+run() {
+    local host=$1
 
     ssh -o ConnectTimeout=1 $host "echo zoo > /dev/null 2>&1" > /dev/null 2>&1
     test $? -eq 0 || return 1
@@ -19,23 +37,19 @@ update_dotfiles() {
     local message
     if [ $result -eq 0 ]; then
         message="$host: dotfiles installed in $destpath"
+        message="$message ... $(update_dotfiles "$host" "$smilepath" "$destpath")"
     else
         message="$host: no dotfiles found"
+        message="$message ... $(update_dotfiles "$host" "$smilepath" "~/${smilepath##*/}")"
     fi
-
-    # rsync -az --delete "$smilepath" "$host:$destpath"
-
-    # if [ $? -eq 0 ]; then
-    #     message="$message ... updated"
-    # else
-    #     message="$message ... not updated, error during rsync"
-    # fi
 
     printf "%s\n" "$message"
 }
 
+export -f update_dotfiles
+
 while read -ru 3 host; do
-    update_dotfiles "$host" &
+    run "$host" &
 done 3<<< "$hosts"
 
 wait
