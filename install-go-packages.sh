a9240c25723d6dc50b72fd61328dc231555b9ed1
Kevin Le Brun <lebrun.k@gmail.com>
2015-06-07 17:41:06 +0200

    Simply install all required Go dependencies

---
+++ install-go-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+go get -v golang.org/x/tools/cmd/...
+go get -v github.com/golang/lint/golint
+go get -v github.com/kisielk/errcheck
+go get -v github.com/cespare/prettybench
+go get -v code.google.com/p/rog-go/exp/cmd/godef
+go get -v github.com/nsf/gocode
+go get -v github.com/kisielk/godepgraph
+go get -v github.com/pkg/profile
+go get -v github.com/jstemmer/gotags
+go get -v github.com/smartystreets/goconvey
+go get -v github.com/mattn/gom
+go get -v github.com/laher/goxc
