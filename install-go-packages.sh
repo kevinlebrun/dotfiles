be68ab8463c9e467c136797c7c04d5db6b923628
Kevin Le Brun <lebrun.k@gmail.com>
2016-05-17 11:38:21 +0200

    Some new shiny tools to my toolbet

--- install-go-packages.sh
+++ install-go-packages.sh
@@ -4,7 +4,7 @@ go get -v golang.org/x/tools/cmd/...
 go get -v github.com/golang/lint/golint
 go get -v github.com/kisielk/errcheck
 go get -v github.com/cespare/prettybench
-go get -v code.google.com/p/rog-go/exp/cmd/godef
+go get -v github.com/rogpeppe/godef
 go get -v github.com/nsf/gocode
 go get -v github.com/kisielk/godepgraph
 go get -v github.com/pkg/profile
@@ -12,3 +12,5 @@ go get -v github.com/jstemmer/gotags
 go get -v github.com/smartystreets/goconvey
 go get -v github.com/mattn/gom
 go get -v github.com/laher/goxc
+go get -v github.com/motemen/gore
+go get -v github.com/mattn/goreman
