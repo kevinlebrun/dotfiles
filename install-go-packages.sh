62938d78c5e4b1a818825b28c6a6d89b7c67984c
Kevin Le Brun <lebrun.k@gmail.com>
2016-11-16 11:37:55 +0100

    Ensure Go packages are updated

--- install-go-packages.sh
+++ install-go-packages.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-go get -v golang.org/x/tools/cmd/...
-go get -v github.com/golang/lint/golint
-go get -v github.com/kisielk/errcheck
-go get -v github.com/cespare/prettybench
-go get -v github.com/rogpeppe/godef
-go get -v github.com/nsf/gocode
-go get -v github.com/kisielk/godepgraph
-go get -v github.com/pkg/profile
-go get -v github.com/jstemmer/gotags
-go get -v github.com/smartystreets/goconvey
-go get -v github.com/mattn/gom
-go get -v github.com/laher/goxc
-go get -v github.com/motemen/gore
-go get -v github.com/mattn/goreman
-go get -v github.com/mholt/caddy
-go get -v github.com/adjust/go-wrk
-go get -v github.com/mholt/caddy
-go get -v github.com/alecthomas/gometalinter
-go get -v github.com/kardianos/govendor
-go get -v github.com/campoy/embedmd
+go get -v -u golang.org/x/tools/cmd/...
+go get -v -u github.com/golang/lint/golint
+go get -v -u github.com/kisielk/errcheck
+go get -v -u github.com/cespare/prettybench
+go get -v -u github.com/rogpeppe/godef
+go get -v -u github.com/nsf/gocode
+go get -v -u github.com/kisielk/godepgraph
+go get -v -u github.com/pkg/profile
+go get -v -u github.com/jstemmer/gotags
+go get -v -u github.com/smartystreets/goconvey
+go get -v -u github.com/mattn/gom
+go get -v -u github.com/laher/goxc
+go get -v -u github.com/motemen/gore
+go get -v -u github.com/mattn/goreman
+go get -v -u github.com/mholt/caddy
+go get -v -u github.com/adjust/go-wrk
+go get -v -u github.com/mholt/caddy
+go get -v -u github.com/alecthomas/gometalinter
+go get -v -u github.com/kardianos/govendor
+go get -v -u github.com/campoy/embedmd
+go get -v -u github.com/y0ssar1an/q
